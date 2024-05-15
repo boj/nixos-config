71e46ac9ec26beb9db680440a1a76b4113b605f4
Brian Jones <bojo@bojo.wtf>
2024-05-14 16:40:05 -0800

    switch up waybar to fancy curve things

--- home/wayland/hyprland/rules.nix
+++ home/wayland/hyprland/rules.nix
@@ -1,5 +1,17 @@
 {
   wayland.windowManager.hyprland.settings = {
+    workspace = [
+      "1, persistent:true, monitor:DP-1"
+      "2, persistent:true, monitor:DP-1"
+      "3, persistent:true, monitor:DP-1"
+      "4, persistent:true, monitor:DP-1"
+      "5, persistent:true, monitor:DP-1"
+      "6, persistent:true, monitor:DP-3"
+      "7, persistent:true, monitor:DP-3"
+      "8, persistent:true, monitor:DP-3"
+      "9, persistent:true, monitor:DP-3"
+      "10, persistent:true, monitor:DP-3"
+    ];
     windowrulev2 = [
       "workspace 6 silent,title:^(.*Discord)$"
       "stayfocused,class:(steam),title:(^$)"
--- home/wayland/waybar.nix
+++ home/wayland/waybar.nix
@@ -5,13 +5,13 @@
       mainBar = {
         layer = "top";
         position = "left";
-        width = 40;
         margin-top = 8;
         margin-bottom = 8;
-        spacing = 15;
-        modules-left = ["hyprland/window"];
-        modules-center = ["hyprland/workspaces"];
-        modules-right = ["clock"];
+        margin-left = 8;
+        reload_style_on_change = true;
+        modules-left = ["hyprland/workspaces"];
+        modules-center = ["clock#date" "clock#time"];
+        modules-right = ["pulseaudio/slider" "pulseaudio#percentage"];
         "hyprland/workspaces" = {
           on-click = "activate";
           format = "{icon}";
@@ -28,74 +28,167 @@
             "10" = "0";
           };
         };
-        "hyprland/window" = {
-          format = "{initialTitle}";
-          max-length = 50;
-          rotate = 270;
+        "clock#date" = {
+          format = "{:%a\n%d}";
+          tooltip-format = "<big>{:%Y %B}</big>\n<tt><small>{calendar}</small></tt>";
         };
-        "clock" = {
-          format = "{:%R}";
-          tooltip-format = "{:%A, %B, %d, %Y}";
-          tooltip = true;
+        "clock#time" = {
+          format = "{:%I\n%M}";
+        };
+        "pulseaudio" = {
+          format = "{icon}";
+          format-bluetooth = "{icon}";
+          format-muted = "";
+          format-icons = {
+            headphones = "";
+            default = [""];
+          };
+          scroll-step = 1;
+          on-click = "pavucontrol";
+        };
+        "pulseaudio#percentage" = {
+          on-click = "pactl set-sink-mute @DEFAULT_SINK@ toggle";
+          format = "{volume}%";
+        };
+        "pulseaudio/slider" = {
+          min = 0;
+          max = 100;
+          orientation = "vertical";
         };
       };
     };
     style = ''
+      @define-color mainbg #${config.colorScheme.palette.base00};
+      @define-color modulesbg #${config.colorScheme.palette.base02};
+      @define-color text #${config.colorScheme.palette.base04};
+      @define-color border #${config.colorScheme.palette.base0F};
+      @define-color buttons #${config.colorScheme.palette.base08};
+      @define-color hover #${config.colorScheme.palette.base09};
+      @define-color white #${config.colorScheme.palette.base05};
+
       * {
-          font-family: Iosevka;
-          font-weight: bold;
-          font-size: 12px;
-          margin: 0;
+        all: initial;
+        font-family:'JetBrainsMono Nerd Font';
+        padding-left: 1px;
+        padding-right: 1px;
       }
 
       window#waybar {
-          background-color: #${config.colorScheme.palette.base02};
-          border-top-right-radius: 12px;
-          border-bottom-right-radius: 12px;
+      	background: @mainbg;
+      	border: 1px solid @border;
+      	border-radius: 5px;
       }
 
-      window#waybar.hidden {
-        opacity: 0;
+      tooltip {
+        background: #${config.colorScheme.palette.base03};
+        border: 2px solid #${config.colorScheme.palette.base0C};
+        border-radius: 5px;
+        font-size: 12pt;
       }
 
-      #window {
-        padding: 100px 20px;
-        color: #${config.colorScheme.palette.base0A};
+      tooltip label {
+        padding: 10px;
+      }
+
+      #workspaces {
+        margin: 10px 0px;
+        background: @modulesbg;
+        font-size: 12pt;
+        padding: 20px 0px;
+        border-top:3px solid @border;
+        border-top-left-radius: 50px;
+        border-top-right-radius: 5px;
+        border-bottom: 3px solid @border;
+        border-bottom-right-radius: 50px;
+        border-bottom-left-radius: 5px;
       }
 
       #workspaces button {
-        padding: 10px 0px;
-        background-color: transparent;
-        color: #${config.colorScheme.palette.base06};
-      }
-
-      #worksapces button.persistent {
-        padding: 10px 0px;
-        background-color: transparent;
-        color: #${config.colorScheme.palette.base05};
-     }
-
-      #workspaces button.hover {
-          color: #${config.colorScheme.palette.base09};
+        padding: 2px;
+        margin: 5px 6px;
       }
 
       #workspaces button.active {
-          color: #${config.colorScheme.palette.base08};
+        background-color: @buttons;
+        font-size: 18pt;
+        border-top-right-radius: 5px;
+        border-top-left-radius: 50px;
+        border-bottom-right-radius: 50px;
+        border-bottom-left-radius: 5px;
       }
 
-      #clock {
-          margin-bottom: 20px;
-          font-family: Iosevka Slab;
-          font-weight: normal;
-          color: #${config.colorScheme.palette.base05};
+      #workspaces button:hover {
+        background: inherit;
+        background-color: @hover;
+        border-top-right-radius: 5px;
+        border-top-left-radius: 50px;
+        border-bottom-right-radius: 50px;
+        border-bottom-left-radius: 5px;
       }
 
-      .modules-left > widget:first-child > #workspaces {
-          margin-left: 0;
+      #pulseaudio, #clock, #clock.date, #clock.time {
+        background: @modulesbg;
       }
 
-      .modules-right > widget:last-child > #workspaces {
-          margin-right: 0;
+      #pulseaudio {
+        padding: 1px 5px 1px 5px;
+        color: @text;
+        font-size: 16px;
+      }
+
+      #clock.date {
+        padding: 20px 5px 10px 5px;
+        font-size: 14px;
+        color: @text;
+        border-top:3px solid @border;
+        border-top-left-radius: 50px;
+        border-top-right-radius: 5px;
+      }
+
+      #clock.time {
+        padding: 10px 5px 20px 5px;
+        font-size: 14px;
+        color: @text;
+        border-bottom: 3px solid @border;
+        border-bottom-left-radius: 5px;
+        border-bottom-right-radius: 50px;
+      }
+
+      #pulseaudio-slider, #pulseaudio.percentage {
+        background-color: @modulesbg;
+      }
+
+      #pulseaudio-slider {
+        padding: 15px 0px 5px 0px;
+        border-top: 3px solid @border;
+        border-top-left-radius: 50px;
+        border-top-right-radius: 5px;
+      }
+      #pulseaudio-slider slider {
+          min-height: 0px;
+          min-width: 1px;
+          opacity: 0;
+          background-image: none;
+          border: none;
+          box-shadow: none;
+      }
+      #pulseaudio-slider trough {
+          min-height: 70px;
+          min-width: 0px;
+          border-radius: 5px;
+          background-color: @border;
+      }
+      #pulseaudio-slider highlight {
+          min-width: 10px;
+          border-radius: 5px;
+          background-color: @white;
+      }
+
+      #pulseaudio.percentage {
+        font-size: 12px;
+        margin: 0px 0px;
+        padding: 2px 5px;
+        color: @text;
       }
     '';
   };
