3205d1b13cde594ef7d17ade78b35568d5a180cc
Brian Jones <bojo@bojo.wtf>
2026-04-01 13:06:36 -0800

    nvidia driver + cuda

--- modules/nixos/gpu.nix
+++ modules/nixos/gpu.nix
@@ -1,7 +1,29 @@
-{ lib, ... }: {
+{ config, lib, pkgs, ... }: let
+  cfg = config.my.gpu;
+in {
   options.my.gpu = lib.mkOption {
     type = lib.types.enum ["amd" "nvidia" "none"];
     default = "none";
     description = "GPU vendor, used to select ROCm vs CUDA package variants.";
   };
+
+  config = lib.mkMerge [
+    (lib.mkIf (cfg == "nvidia") {
+      services.xserver.videoDrivers = ["nvidia"];
+      hardware.nvidia = {
+        modesetting.enable = true;
+        open = true;
+        nvidiaSettings = true;
+        package = config.boot.kernelPackages.nvidiaPackages.stable;
+      };
+      hardware.graphics.enable = true;
+      environment.systemPackages = [
+        config.boot.kernelPackages.nvidiaPackages.stable
+        pkgs.cudaPackages.cudatoolkit
+      ];
+    })
+    (lib.mkIf (cfg == "amd") {
+      hardware.graphics.enable = true;
+    })
+  ];
 }
